11a152289c07893bf0a20e061fefd15108560ceb
GeorgeKarlinzer <georg11.01@mail.ru>
2022-06-14 00:32:24 +0200

    Add data generator

---
+++ DB/DB/DataBaseService.cs
@@ -0,0 +1,278 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    internal sealed class DataBaseService
+    {
+        private readonly string connectionString = @"Server=dbmanage.lab.ii.agh.edu.pl;Database=u_nstaron;User Id=;Password=;";
+        private SqlConnection conn;
+
+        public void OpenConnection()
+        {
+            conn = new SqlConnection(connectionString);
+            conn.Open();
+        }
+
+        public void CloseConnection()
+        {
+            conn.Close();
+        }
+
+        public bool AddClient(out string errorMsg, string clientName, string region, string city, string address, string NIP)
+        {
+            try
+            {
+                using var cmd = new SqlCommand("dbo.AddClient", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new("ClientName", clientName));
+                cmd.Parameters.Add(new("Region", region));
+                cmd.Parameters.Add(new("City", city));
+                cmd.Parameters.Add(new("Address", address));
+                cmd.Parameters.Add(new("NIP", NIP));
+                cmd.Parameters.Add("Result", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                var resCode = cmd.ExecuteNonQuery();
+                errorMsg = (string)cmd.Parameters["Result"].Value;
+                return resCode == 0;
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool AddConference(out string errorMsg, string name, List<DayList> days, int capacity, float price, string region, string city, string address)
+        {
+            try
+            {
+                using var cmd = new SqlCommand("dbo.AddConference", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new("Name", name));
+                cmd.Parameters.Add(new("Region", region));
+                cmd.Parameters.Add(new("Capacity", capacity));
+                cmd.Parameters.Add(new("Price", price));
+                cmd.Parameters.Add(new("Address", address));
+                cmd.Parameters.Add(new("City", city));
+
+                cmd.Parameters.Add(new("Days", SqlDbType.Structured));
+                IEnumerable<SqlDataRecord> enumerable = days.Select((c, i) => c.ToSqlDataRecord(i + 1));
+                var a = enumerable.ToList();
+                cmd.Parameters["Days"].Value = enumerable;
+                cmd.Parameters["Days"].TypeName = "dbo.DayList";
+
+                cmd.Parameters.Add("Result", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                var resCode = cmd.ExecuteNonQuery();
+                errorMsg = (string)cmd.Parameters["Result"].Value;
+                return resCode == 0;
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool AddConferenceParticipant(out string errorMsg, int participantId, int reservationId)
+        {
+            try
+            {
+                using var cmd = new SqlCommand("dbo.AddConferenceParticipant", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new("ParticipantId", participantId));
+                cmd.Parameters.Add(new("ReservationId", reservationId));
+                cmd.Parameters.Add("Result", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                var resCode = cmd.ExecuteNonQuery();
+                errorMsg = (string)cmd.Parameters["Result"].Value;
+                return resCode == 0;
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool AddParticipant(out string errorMsg, string fn, string ln, string reg, string city, string add, DateTime bd)
+        {
+            try
+            {
+                using var cmd = new SqlCommand("dbo.AddParticipant", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new("FirstName", fn));
+                cmd.Parameters.Add(new("LastName", ln));
+                cmd.Parameters.Add(new("Region", reg));
+                cmd.Parameters.Add(new("City", city));
+                cmd.Parameters.Add(new("Address", add));
+                cmd.Parameters.Add(new("BirthDate", bd));
+                cmd.Parameters.Add("Result", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                var resCode = cmd.ExecuteNonQuery();
+                errorMsg = (string)cmd.Parameters["Result"].Value;
+                return resCode == 0;
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool AddReservation(out string errorMsg, int? cdId, int? wId, int clientId)
+        {
+            try
+            {
+                using var cmd = new SqlCommand("dbo.AddReservation", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new("ConferenceDayId", (object)cdId ?? DBNull.Value));
+                cmd.Parameters.Add(new("WorkshopId", (object)wId ?? DBNull.Value));
+                cmd.Parameters.Add(new("ClientId", clientId));
+                cmd.Parameters.Add("Result", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                var resCode = cmd.ExecuteNonQuery();
+                errorMsg = (string)cmd.Parameters["Result"].Value;
+                return resCode == 0;
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool AddWorkshop(out string errorMsg, string name, TimeSpan sTime, TimeSpan fTime, int capacity, int price, string region, string city, string address, int cdId)
+        {
+            try
+            {
+                using var cmd = new SqlCommand("dbo.AddWorkshop", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new("Name", name));
+                cmd.Parameters.Add(new("StartTime", sTime));
+                cmd.Parameters.Add(new("FinishTime", fTime));
+                cmd.Parameters.Add(new("Capacity", capacity));
+                cmd.Parameters.Add(new("Price", price));
+                cmd.Parameters.Add(new("Region", region));
+                cmd.Parameters.Add(new("City", city));
+                cmd.Parameters.Add(new("Address", address));
+                cmd.Parameters.Add(new("ConferenceDayId", cdId));
+                cmd.Parameters.Add("Result", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                var resCode = cmd.ExecuteNonQuery();
+                errorMsg = (string)cmd.Parameters["Result"].Value;
+                return resCode == 0;
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool AddWorkshopParticipant(out string errorMsg, int cpId, int reservationId)
+        {
+            try
+            {
+                using var cmd = new SqlCommand("dbo.AddWorkshopParticipant", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new("ConferenceParticipant", cpId));
+                cmd.Parameters.Add(new("ReservationId", reservationId));
+                cmd.Parameters.Add("Result", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                var resCode = cmd.ExecuteNonQuery();
+                errorMsg = (string)cmd.Parameters["Result"].Value;
+                return resCode == 0;
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool MakePayment(out string errorMsg, int amount, int reservationId, DateTime date)
+        {
+            try
+            {
+                using var cmd = new SqlCommand("dbo.MakePayment", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new("Amount", amount));
+                cmd.Parameters.Add(new("ReservationId", reservationId));
+                cmd.Parameters.Add(new("Date", date));
+                cmd.Parameters.Add("Result", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                var resCode = cmd.ExecuteNonQuery();
+                errorMsg = (string)cmd.Parameters["Result"].Value;
+                return resCode == 0;
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool ClearData()
+        {
+            try
+            {
+                using var cmd = new SqlCommand("debug.RecreateTables", conn);
+
+                var resCode = cmd.ExecuteNonQuery();
+                return resCode == 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public SqlDataReader ExecuteQuery(string query)
+        {
+            using var cmd = new SqlCommand(query, conn);
+            var reader = cmd.ExecuteReader();
+            return reader;
+        }
+
+
+        public class DayList
+        {
+            public DateTime Date { get; set; }
+            public TimeSpan StartTime { get; set; }
+            public TimeSpan FinishTime { get; set; }
+
+            private static readonly SqlMetaData[] myRecordSchema = {
+                new SqlMetaData("Id", SqlDbType.Int),
+                new SqlMetaData("Date", SqlDbType.Date),
+                new SqlMetaData("Start Time", SqlDbType.Time),
+                new SqlMetaData("Finish Time", SqlDbType.Time)
+            };
+
+            public SqlDataRecord ToSqlDataRecord(int id)
+            {
+                var record = new SqlDataRecord(myRecordSchema);
+                record.SetInt32(0, id);
+                record.SetDateTime(1, Date);
+                record.SetTimeSpan(2, StartTime);
+                record.SetTimeSpan(3, FinishTime);
+                return record;
+            }
+        }
+    }
+}
---
+++ DB/DB/Program.cs
@@ -0,0 +1,105 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DB;
+
+var dbService = new DataBaseService();
+dbService.OpenConnection();
+
+dbService.ClearData();
+
+dbService.AddClient(out _, "Client 1", "Region", "City", "Address", "NIP");
+dbService.AddClient(out _, "Client 2", "Region", "City", "Address", "NIP");
+
+var days = new List<DataBaseService.DayList>()
+{
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-07-14"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-07-15"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-07-16"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-07-17"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-07-18"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+
+};
+dbService.AddConference(out _, "Conf 1", days, 100, 2034.5f, "Region", "City", "Address");
+
+days = new List<DataBaseService.DayList>()
+{
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-08-14"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-08-15"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+
+};
+dbService.AddConference(out _, "Conf 2", days, 200, 1000, "Region", "City", "Address");
+
+days = new List<DataBaseService.DayList>()
+{
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-09-14"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-09-15"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-09-16"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+    new DataBaseService.DayList() { Date = DateTime.Parse("2022-09-17"), StartTime = TimeSpan.Parse("12:00:00"), FinishTime = TimeSpan.Parse("17:00:00") },
+
+};
+dbService.AddConference(out _, "Conf 3", days, 70, 10000, "Region", "City", "Address");
+
+for (int i = 0; i < 11; i++)
+    AddWorkshops(2, 30, i + 1);
+
+AddParticipant(500);
+AddReservationsConference(200, 11, 2);
+AddReservationWorkshop(300, 22, 2);
+AddConferenceParticipants(200);
+
+for (int i = 0; i < 10; i++)
+    AddWorkshopParticipants(200, 201);
+
+for (int i = 0; i < 10; i++)
+    MakePayments(500);
+
+
+void AddWorkshops(int count, int capacity, int cdId)
+{
+    for (int i = 0; i < count; i++)
+        dbService.AddWorkshop(out _, "W 1", TimeSpan.Parse("13:00:00"), TimeSpan.Parse("16:00:00"), capacity, 1000, "Reg", "City", "Add", cdId);
+}
+
+void AddParticipant(int count)
+{
+    for (int i = 0; i < count; i++)
+        dbService.AddParticipant(out _, RandomString(5), RandomString(5), "Reg", "City", "Add", DateTime.Now);
+}
+
+void AddReservationsConference(int count, int cdCount, int clientCount)
+{
+    for (int i = 0; i < count; i++)
+        dbService.AddReservation(out _, new Random().Next(1, cdCount), null, new Random().Next(1, clientCount + 1));
+}
+
+void AddReservationWorkshop(int count, int wCount, int clientCount)
+{
+    for (int i = 0; i < count; i++)
+        dbService.AddReservation(out _, null, new Random().Next(1, wCount), new Random().Next(1, clientCount + 1));
+}
+
+void AddConferenceParticipants(int count)
+{
+    for (int i = 0; i < count; i++)
+        dbService.AddConferenceParticipant(out _, i + 1, i + 14);
+}
+
+void AddWorkshopParticipants(int count, int resStart)
+{
+    for (int i = 0; i < count; i++)
+        dbService.AddWorkshopParticipant(out _, new Random().Next(1, count), resStart + i);
+}
+
+void MakePayments(int count)
+{
+    for (int i = 0; i < count; i++)
+        dbService.MakePayment(out _, new Random().Next(1, 100), new Random().Next(0, count), DateTime.Now);
+}
+
+dbService.CloseConnection();
+
+string RandomString(int length)
+{
+    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return new string(Enumerable.Repeat(chars, length)
+        .Select(s => s[new Random().Next(s.Length)]).ToArray());
+}
